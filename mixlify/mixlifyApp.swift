2ab07c23123ab27355997afe05d764e99645a3c1
Qveys <quentinveys@gmail.com>
2025-03-24 10:28:31 +0100

    Initial App

---
+++ mixlify/mixlifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixlifyApp.swift
+//  mixlify
+//
+//  Created by Quentin VEYS on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct mixlifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mixlifyTests/mixlifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  mixlifyTests.swift
+//  mixlifyTests
+//
+//  Created by Quentin VEYS on 24/03/2025.
+//
+
+import Testing
+
+struct mixlifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
